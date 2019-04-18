c601c2fdb4df8437767e9673f02931c335b0abf8
Jason Breck <jbreck@cs.wisc.edu>
2019-04-17 19:40:57 -0500

    Committing prior changes

--- duet/craNonLinRec.ml
+++ duet/craNonLinRec.ml
@@ -34,7 +34,7 @@ end)
 
 (* ugly: copied from newtonDomain just for debugging *)
 let print_indented indent k =
-      Printf.printf "%s"
+      Format.printf "%s"
       (SrkUtil.mk_show (fun formatter tr ->
           Format.pp_open_vbox formatter indent;
           Format.pp_print_break formatter 0 0;
@@ -90,7 +90,11 @@ let post_symbol =
   Memo.memo (fun sym ->
       match Var.of_symbol sym with
       | Some var ->
-        Srk.Syntax.mk_symbol Cra.srk ~name:("postrec_" ^ Var.show var) (Var.typ var :> Srk.Syntax.typ)
+        let newsym = Srk.Syntax.mk_symbol Cra.srk ~name:("postrec_" ^ Var.show var) (Var.typ var :> Srk.Syntax.typ) in
+        (*Format.printf "New Symbol Created!  old=%s  new=%s\n" 
+          (Srk.Syntax.show_symbol Cra.srk sym)
+          (Srk.Syntax.show_symbol Cra.srk newsym); *)
+        newsym
       | None -> assert false)
 
 let to_transition_formula tr =
@@ -120,48 +124,123 @@ let of_transition_formula tr_symbols fmla =
     in
     K.construct fmla transform
 
+type 'a bound_info = {
+  bound_pairs : (Srk.Syntax.symbol * 'a Srk.Syntax.term) list;
+  recursion_flag : Cra.value;
+  call_abstraction : K.t
+}
+
+let assign_value_to_literal value literal = 
+  K.assign value (Srk.Syntax.mk_real Cra.srk (Srk.QQ.of_int literal))
+
+let assume_value_eq_literal value literal = 
+  let var = Cra.V.symbol_of value in 
+  K.assume (Srk.Syntax.mk_eq Cra.srk 
+    (Srk.Syntax.mk_const Cra.srk var)
+    (Srk.Syntax.mk_real Cra.srk (Srk.QQ.of_int literal)))
+
+(* PASS REC FLAG IN FROM OUTSIDE *)
+(* Make bound analysis? *)
 let mk_call_abstraction base_case_weight = 
   let (tr_symbols, body) = to_transition_formula base_case_weight in
-  Format.printf "  transition_formula_body: \n%a \n" (Srk.Syntax.Formula.pp Cra.srk) body;
+  (*Format.printf "  transition_formula_body: \n%a \n" (Srk.Syntax.Formula.pp Cra.srk) body;*)
   let projection x = 
-    (* FIXME: THIS IS QUICK AND VERY DIRTY *)
+    (* FIXME: THIS IS QUICK AND DIRTY *)
     (List.fold_left (fun found (vpre,vpost) -> found || vpre == x || vpost == x) false tr_symbols)
     || 
     match Cra.V.of_symbol x with 
-    | Some v -> true (* Cra.V.is_global v *)
+    | Some v -> Cra.V.is_global v
     | None -> false (* false *)
   in 
   let wedge = Wedge.abstract ~exists:projection Cra.srk body in 
   Format.printf "\n  base_case_wedge = %t \n\n" (fun f -> Wedge.pp f wedge);
   let cs = Wedge.coordinate_system wedge in 
-  let make_bounding_var t = 
-      let term = CoordinateSystem.term_of_vec cs t in 
-      Format.printf "  base-case-bounded term: %a \n" (Srk.Syntax.Term.pp Cra.srk) term;
-    let bounding_var = Core.Var.mk (Core.Varinfo.mk_global "B" ( Core.Concrete (Core.Int 32))) in 
+  let bounding_atoms = ref [] in
+  let bound_list = ref [] in
+  let add_bounding_var vec negate =
+      let vec = if negate then Srk.Linear.QQVector.negate vec else vec in 
+      let term = CoordinateSystem.term_of_vec cs vec in 
+      (*Format.printf "  base-case-bounded term: %a \n" (Srk.Syntax.Term.pp Cra.srk) term;*)
+    let bounding_var = Core.Var.mk (Core.Varinfo.mk_global "B_in" ( Core.Concrete (Core.Int 32))) in 
     let bounding_var_sym = Cra.V.symbol_of (Cra.VVal bounding_var) in 
-    (*let bounding_var_sym = Srk.Syntax.mk_symbol Cra.srk ~name:"B" `TyInt in *)
+    (*let bounding_var_sym = Srk.Syntax.mk_symbol Cra.srk ~name:"B_in" `TyInt in *)
     let bounding_term = Srk.Syntax.mk_const Cra.srk bounding_var_sym in 
     let bounding_atom = Srk.Syntax.mk_leq Cra.srk term bounding_term in 
-    bounding_atom in
-    (* PROBABLY KEEP TRACK OF SEVERAL OF THESE THINGS... *)
-  let bounding_atoms = ref [] in
-  let analyze_recurrence = 
-    function
-    | (`Eq, t) ->
-      Format.printf "EQ\n";
-      bounding_atoms := (make_bounding_var t) :: (!bounding_atoms) ;
-      (* NEED TO DO NEGATED ONE ALSO *)
-      ()
-      (*Format.printf "  rec equation: %a \n" Linear.QQVector.pp t;*)
-    | (`Geq, t) ->
-      Format.printf "GEQ\n";
-      (* NEED TO DO NEGATED ONE ONLY *)
-      ()
-  in
-  List.iter analyze_recurrence (Wedge.polyhedron wedge);
+    bounding_atoms := bounding_atom            :: (!bounding_atoms);
+    bound_list     := (bounding_var_sym, term) :: (!bound_list) in
+  let handle_constraint = function 
+    | (`Eq, vec) ->
+      (*Format.printf "  rec equation: %a \n" Linear.QQVector.pp vec;*)
+      add_bounding_var vec true; 
+      add_bounding_var vec false
+    | (`Geq, vec) ->
+      add_bounding_var vec true in 
+  List.iter handle_constraint (Wedge.polyhedron wedge);
+  let rec_flag_var = Core.Var.mk (Core.Varinfo.mk_global "Recursion_Flag" ( Core.Concrete (Core.Int 32))) in 
+  let rec_flag_val = Cra.VVal rec_flag_var in 
+  let rec_flag_var_sym = Cra.V.symbol_of rec_flag_val in (* Add to symbol table... (HACK!) *)
+  let set_rec_flag = assign_value_to_literal rec_flag_val 1 in 
   let call_abstraction_fmla = Srk.Syntax.mk_and Cra.srk (!bounding_atoms) in 
-  of_transition_formula tr_symbols call_abstraction_fmla
+  let call_abstraction_weight = of_transition_formula tr_symbols call_abstraction_fmla in 
+    {bound_pairs = !bound_list;
+     recursion_flag = rec_flag_val; 
+     call_abstraction = K.mul set_rec_flag call_abstraction_weight}
 
+let mk_height_based_summary bounds recursive_weight = 
+  (* REMOVE BASE CASE? *)
+  let initially_no_recursion = (assign_value_to_literal bounds.recursion_flag 0) in 
+  let eventually_recursion = (assume_value_eq_literal bounds.recursion_flag 1) in 
+  let recursive_weight = 
+    K.mul (K.mul initially_no_recursion recursive_weight) eventually_recursion in
+  (* ASSUME B_in NON-NEGATIVE*)
+  let (tr_symbols, body) = to_transition_formula recursive_weight in
+  Format.printf "  transition_formula_body: \n%a \n" (Srk.Syntax.Formula.pp Cra.srk) body;
+  let b_out_definitions = ref [] in 
+  let b_in_b_out_pairs = ref [] in
+  let add_b_out_definition (inner_sym, term) =
+      (* I'm 90% sure this should be a different symbol *)
+      let outer_sym = Srk.Syntax.mk_symbol Cra.srk ~name:"B_out" `TyInt in
+      let lhs = Srk.Syntax.mk_const Cra.srk outer_sym in 
+      (*let lhs = Srk.Syntax.mk_const Cra.srk sym in *) 
+      let rhs = term in 
+      let equation = Srk.Syntax.mk_eq Cra.srk lhs rhs in 
+      Format.printf "  bounded term: %a ~ %t ~ %t @." 
+        (Srk.Syntax.Term.pp Cra.srk) term
+        (fun f -> Srk.Syntax.pp_symbol Cra.srk f inner_sym)
+        (fun f -> Srk.Syntax.pp_symbol Cra.srk f outer_sym);
+      b_out_definitions := equation :: (!b_out_definitions);
+      b_in_b_out_pairs := (inner_sym, outer_sym) :: (!b_in_b_out_pairs) in 
+  List.iter add_b_out_definition bounds.bound_pairs;
+  let b_out_conjunction = Srk.Syntax.mk_and Cra.srk (!b_out_definitions) in 
+  (*Format.printf "  b_out_conjunction: \n%a \n" (Srk.Syntax.Formula.pp Cra.srk) b_out_conjunction;*)
+  let full_conjunction = Srk.Syntax.mk_and Cra.srk [body; b_out_conjunction] in 
+    let projection x = 
+    (* FIXME: THIS IS QUICK AND DIRTY *)
+    (List.fold_left (fun found (inner_sym,outer_sym) -> 
+      found || inner_sym == x || outer_sym == x) false (!b_in_b_out_pairs))
+    
+    (*(List.fold_left (fun found (vpre,vpost) -> found || vpost == x) false tr_symbols)*)
+    (*|| 
+    match Cra.V.of_symbol x with 
+    | Some v -> Cra.V.is_global v
+    | None -> false (* false *)
+    *)
+  in 
+  let wedge = Wedge.abstract ~exists:projection Cra.srk full_conjunction in 
+  Format.printf "\n  extraction_wedge = @.%t@. \n\n" (fun f -> Wedge.pp f wedge);
+  
+  (* *)
+  (*  - Create B_out variables
+      - Associate each B_out variable to the appropriate outer-call term
+      ? How do I get things connected properly to pre- and post- variables?
+      - Build the inequation atom for each B_out and its term
+      - Make recursive_weight into a transition formula
+      - Conjoin these transition formulas
+      - Make a wedge out of the formula, projecting down to B_outs and B_ins
+      - Walk over the B_out inequations, dump ones that break some rules
+
+      * Consider: Make a second set of variables for the unchanging bounds?  *)
+  ()
 (*
     let mul left right =
     let fresh_skolem =
@@ -205,7 +284,7 @@ let analyze_nonlinrec file =
         (* let open CfgIr in let file = get_gfile() in *)
         K.havoc (List.map (fun vi -> Cra.VVal (Core.Var.mk vi)) file.vars) in
       let summarizer (scc : BURG.scc) path_weight_internal = 
-        print_endline "I saw an SCC:";
+        Format.printf "I saw an SCC:\n";
         List.iter (fun (u,v,p) -> (Format.printf "  Proc(%d,%d) = %t \n" u v (fun f -> Pathexpr.pp f p))) scc.procs;
         List.iter (fun (p_entry,p_exit,pathexpr) ->
             let pe_call_edge_set = function
@@ -243,18 +322,22 @@ let analyze_nonlinrec file =
             print_indented 15 base_case_weight;
             Format.printf "  ]\n";
             (* *)
-            let call_abstraction = mk_call_abstraction base_case_weight in 
+            let bounds = mk_call_abstraction base_case_weight in 
             Format.printf "  call_abstration = [";
-            print_indented 15 call_abstraction;
+            print_indented 15 bounds.call_abstraction;
             Format.printf "  ]\n";
             let weight_of_call_rec cs ct cen cex = 
-              if cen == p_entry && cex == p_exit then call_abstraction
+              if cen == p_entry && cex == p_exit then bounds.call_abstraction
               else failwith "Mutual recursion not implemented"
             in
             let recursive_weight = path_weight_internal p_entry p_exit weight_of_call_rec in
             Format.printf "  recursive_weight = [";
             print_indented 15 recursive_weight;
             Format.printf "  ]\n";
+            let height_based_summary = mk_height_based_summary bounds recursive_weight in
+            (* *)
+            (*let bounding_var_sym = Srk.Syntax.mk_symbol Cra.srk ~name:"Bpost" `TyInt in *)
+            (* *)
             (* WHICH VARIABLES TO PROJECT OUT? *)
             (*
             let atom_evaluator (formula_node : ('x, 'y) Srk.Syntax.open_formula) = 
