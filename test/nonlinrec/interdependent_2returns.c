8025e73808b5533a3a16b2c669813aa88dbba1e4
Jason Breck <jbreck@cs.wisc.edu>
2019-05-27 17:23:43 -0500

    Adding interdependence example

---
+++ test/nonlinrec/interdependent_2returns.c
@@ -0,0 +1,24 @@
+int __cost;
+int return1;
+int return2;
+void recursive(int n) {
+    if (n == 1) { return1 = 1; return2 = 1; return; }
+    recursive(n-1);
+    int temp_return1_a = return1;
+    int temp_return2_a = return2;
+    recursive(n-1);
+    int temp_return1_b = return1;
+    int temp_return2_b = return2;
+    //return2 = (temp_return1_a + temp_return1_b) * 1;
+    //return1 = (temp_return2_a + temp_return2_b) * 1;
+    return2 = (temp_return1_a) * 2;
+    return1 = (temp_return2_a) * 2;
+    return;
+}
+void main(int n) {
+    return1 = 1;
+    return2 = 1;
+    __VERIFIER_assume(n >= 0);
+    recursive(n);
+    __cost = return1;
+}
