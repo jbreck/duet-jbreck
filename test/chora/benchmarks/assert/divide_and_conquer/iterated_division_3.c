93e6217960faa29e38251086152f79bfb02a87fa
Jason Breck <jbreck@cs.wisc.edu>
2019-06-07 12:36:31 -0500

    Adding iterated_division_3.c

---
+++ test/chora/benchmarks/assert/divide_and_conquer/iterated_division_3.c
@@ -0,0 +1,17 @@
+int __cost;
+void main(int n) {
+  int m = 1;
+  //int p = 1;
+  __cost = 0;
+  __VERIFIER_assume(n > 4);
+  while(n != 4) {
+    //p = p - 7;
+    __VERIFIER_assume(n > 400);
+    //__VERIFIER_assume(n > 0);
+    n = n / 2;
+    //n = n - 1;
+    __cost++;
+    m = 3 * m;
+  }
+  __cost = m;
+}
