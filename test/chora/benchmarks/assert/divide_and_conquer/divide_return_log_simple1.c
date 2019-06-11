fcea34ee09f625effd71f019b72eca3776a83758
Jason Breck <jbreck@cs.wisc.edu>
2019-06-10 20:30:50 -0500

    Added an log-bound assertion I'd like to be able to prove

---
+++ test/chora/benchmarks/assert/divide_and_conquer/divide_return_log_simple1.c
@@ -0,0 +1,24 @@
+int lg_n_helper(int n) {
+    int i;
+    int r = 0;
+    int j;
+    //__VERIFIER_assume(n > 0);
+    for(i = 1; i != n; i *= 2) {
+        r ++;
+    }
+    return r;
+}
+int recursive(int n) {
+    //__VERIFIER_assume(n >= 2);
+    if (n == 2) { return 0; }
+    recursive(n/2);
+    return recursive(n/2) + 1;
+}
+void main(int n) {
+    //__VERIFIER_assume(n > 2);
+    int final = recursive(n);
+    int bound = lg_n_helper(n);
+    if (__VERIFIER_nondet_int()) {
+        __VERIFIER_assert(final <= bound);
+    }
+}
