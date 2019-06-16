d3db9a4f5dfa15bc513cbf7864f9875d6be050d8
Jason Breck <jbreck@cs.wisc.edu>
2019-06-16 15:53:02 -0500

    Renamed retval in findloop benchmarks

--- test/chora/benchmarks/other/findloop_rba_return.c
+++ test/chora/benchmarks/other/findloop_rba_return.c
@@ -4,13 +4,13 @@ int find(int * A, int n) { return findAux(A,0,n,0,0); }
 int findAux(int * A, int i, int n, int sum, int nAdded) {
     nTicks++;
     if (i >= n) { if (sum == 0 && nAdded > 0) {return 1;} else {return 0;} }
-    int retval = 0;
+    int nFound = 0;
     for(int tries = 0; tries < 2; tries++) {
-        retval += findAux(A,i+1,n,sum,nAdded); // first try does not include A[i]
-        if (retval != 0) break;
+        nFound += findAux(A,i+1,n,sum,nAdded); // first try does not include A[i]
+        if (nFound != 0) break;
         sum += A[i]; nAdded += 1; // second try does include A[i]
     }
-    return retval;
+    return nFound;
 }
 void main(int n) {
     int * A;
