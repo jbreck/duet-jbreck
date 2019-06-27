789a19cec40b6b37ed4237440019c1727065d682
Jason Breck <jbreck@cs.wisc.edu>
2019-06-27 15:08:13 -0500

    Adding overview example subsetsum_size_overview.c

---
+++ test/chora/benchmarks/other/subsetsum_size_overview.c
@@ -0,0 +1,24 @@
+#define bool int
+#define false 0
+#define true 1
+int __cost;
+int nTicks;
+bool found;
+int subsetSumSize(int * A, int n) { found = false; return subsetSumSizeAux(A,0,n,0); }
+int subsetSumSizeAux(int * A, int i, int n, int sum) {
+    nTicks++;
+    if (i >= n) {
+        if (sum == 0) { found = true; }
+        return 0;
+    }
+    int size = subsetSumSizeAux(A,i+1,n,sum + A[i]); // add A[i]
+    if (found) return size + 1;
+    size = subsetSumSizeAux(A,i+1,n,sum); // skip A[i]
+    return size;
+}
+void main(int n) {
+    int * A;
+    //if (n >= 0) {
+        __cost = subsetSumSize(A,n);
+    //}
+}
