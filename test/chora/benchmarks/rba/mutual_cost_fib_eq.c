3ef030c49505ca69c7689c7e4942a16d7412ad0b
Jason Breck <jbreck@cs.wisc.edu>
2019-10-28 18:59:17 -0500

    Bugfix to mutual Fibonacci program in rba directory

--- test/chora/benchmarks/rba/mutual_cost_fib_eq.c
+++ test/chora/benchmarks/rba/mutual_cost_fib_eq.c
@@ -1,13 +1,13 @@
 int __cost;
 
 void fib1(int n) {
-   if (n == 1) {__cost++; return;}
+   if (n == 0 || n == 1) {__cost++; return;}
    fib2(n-1);
    fib2(n-2);
 }
 
 void fib2(int n) {
-   if (n == 1) {__cost++; return;}
+   if (n == 0 || n == 1) {__cost++; return;}
    fib1(n-1);
    fib1(n-2);
 }
