b04cdd8646ade75d2af65c88515d86b45ac495e0
Jason Breck <jbreck@cs.wisc.edu>
2019-10-28 18:54:03 -0500

    Changes to a few testing-script tool definitions

--- test/chora/scripts/tooldefs/tool_chora_summary_dsqueeze.py
+++ test/chora/scripts/tooldefs/tool_chora_summary_dsqueeze.py
@@ -3,4 +3,4 @@ import choraconfig, os.path
 tool = choraconfig.clone_tool("chora")
 tool["displayname"] = "CHORA:sds"
 tool["shortname"] = "chora:sds"
-tool["cmd"] = [choraconfig.parent(2,choraconfig.testroot) + "/duet.native","-chora-summaries","-chora-debug-squeeze","-chora","{filename}"]
+tool["cmd"] = [choraconfig.parent(2,choraconfig.testroot) + "/duet.native","-chora-debug-recs","-chora-summaries","-chora-debug-squeeze","-chora","{filename}"]
