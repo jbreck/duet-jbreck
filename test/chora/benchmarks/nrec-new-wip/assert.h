c4e9d4fbb3f837e8e802b81e3ce76e3cc209cfc3
Jason Breck <jbreck@cs.wisc.edu>
2019-11-21 23:11:57 -0600

    Adding assert.h and unreach-call.prp

---
+++ test/chora/benchmarks/nrec-new-wip/assert.h
@@ -0,0 +1,7 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
+
+void __VERIFIER_assert(int phi) {
+    if (phi) { return; }
+ERROR: __VERIFIER_error();
+}
