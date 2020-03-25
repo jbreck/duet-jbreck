4a66f4d9895f97b41012b2cf2bba3497d67347fe
Jason Breck <jbreck@cs.wisc.edu>
2020-03-25 12:31:59 -0500

    Fix tool name in batch_svrec-all

--- test/chora/scripts/batchdefs/batch_svrec-all.py
+++ test/chora/scripts/batchdefs/batch_svrec-all.py
@@ -2,5 +2,5 @@ import choraconfig, glob
 
 batch = choraconfig.clone_batch("svrec")
 
-batch["toolIDs"] = ["chora2019full","icra2019","ua","utaipan","viap"]
+batch["toolIDs"] = ["chorafull","icra2019","ua","utaipan","viap"]
 
